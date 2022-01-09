38d49514afbd31a766916aadbe82abbb6622bbb0
rahul-e <rahulrajasekharan@yahoo.com>
2022-01-09 11:42:07 +0000

    Added a function to write the forming simulation input file for each design point

--- runAniform.py
+++ runAniform.py
@@ -37,20 +37,20 @@ def read_AniformModel():
 		Param["c0"] = []
 		Param["Ft"] = [] # Film thickness
 		Param["C"] = [] # Another param in Power Law viscosity
-		Param["Eta0"] = []
+		Param["eta0"] = []
 		Param["n"] = []
 		Param["ap"] = []
 		Param["bp"] = []
 		Param["p0"] = []
-		if len(tmpdf['col2'].str.match('PenaltyPolymerILD')):
-			PenPolyNum = tmpdf[tmpdf['col2'].str.match('PenaltyPolymerILD')].index
+		if len(tmpdf['col2'].str.fullmatch('PenaltyPolymerILD')):
+			PenPolyNum = tmpdf[tmpdf['col2'].str.fullmatch('PenaltyPolymerILD')].index
 			for i in PenPolyNum:
 				Param["Ps"].append(tmpdf['col1'].iloc[i+1]) #=tmpdf['col1'].iloc[i+1]+0.1*tmpdf['col1'].iloc[i+1] # Penalty stiffness
 				Param["tau0"].append(tmpdf['col1'].iloc[i+2])
 				Param["c0"].append(tmpdf['col1'].iloc[i+3])
 				Param["Ft"].append(tmpdf['col1'].iloc[i+4])
 				Param["C"].append(tmpdf['col1'].iloc[i+5]) #tmpdf['col1'].iloc[i+5]+0.1*tmpdf['col1'].iloc[i+5] # C
-				Param["Eta0"].append(tmpdf['col1'].iloc[i+6])
+				Param["eta0"].append(tmpdf['col1'].iloc[i+6])
 				Param["n"].append(tmpdf['col1'].iloc[i+7])
 				Param["ap"].append(tmpdf['col1'].iloc[i+8])
 				Param["bp"].append(tmpdf['col1'].iloc[i+9])
@@ -65,18 +65,18 @@ def read_AniformModel():
 		Emi = [] # Emmisivity
 		EmiO = []	# Emissivity Other
 
-		if len(tmpdf['col2'].str.match('ConductanceIT')):
-			CondNum = tmpdf[tmpdf['col2'].str.match('ConductanceIT')].index
+		if len(tmpdf['col2'].str.fullmatch('ConductanceIT')):
+			CondNum = tmpdf[tmpdf['col2'].str.fullmatch('ConductanceIT')].index
 			for i in CondNum:
 				Cond.append(tmpdf['col1'].iloc[i+2])
 
-		if len(tmpdf['col2'].str.match('ConvectionIT')):
-			ConvNum = tmpdf[tmpdf['col2'].str.match('ConvectionIT')].index
+		if len(tmpdf['col2'].str.fullmatch('ConvectionIT')):
+			ConvNum = tmpdf[tmpdf['col2'].str.fullmatch('ConvectionIT')].index
 			for i in ConvNum:
 				Conv.append(tmpdf['col1'].iloc[i+2])
 
-		if len(tmpdf['col2'].str.match('RadiationIT')):
-			RadNum = tmpdf[tmpdf['col2'].str.match('RadiationIT')].index
+		if len(tmpdf['col2'].str.fullmatch('RadiationIT')):
+			RadNum = tmpdf[tmpdf['col2'].str.fullmatch('RadiationIT')].index
 			for i in RadNum:
 				Emi.append(tmpdf['col1'].iloc[i+3])
 				EmiO.append(tmpdf['col1'].iloc[i+4])
@@ -91,10 +91,10 @@ def read_AniformModel():
 		nu12 = []
 		G12 = []
 
-		if len(tmpdf['col2'].str.match('IsoElasticLD')):
-			IsoElasNum = tmpdf[tmpdf['col2'].str.match('IsoElasticLD')].index
+		if len(tmpdf['col2'].str.fullmatch('IsoElasticLD')):
+			IsoElasNum = tmpdf[tmpdf['col2'].str.fullmatch('IsoElasticLD')].index
 			for i in IsoElasNum:
-				if len(tmpdf['col0'].str.match('density')):
+				if len(tmpdf['col0'].str.fullmatch('density')):
 					Rho.append(tmpdf['col1'].iloc[i+1])
 					E.append(tmpdf['col1'].iloc[i+2])
 					nu.append(tmpdf['col1'].iloc[i+3])
@@ -102,8 +102,8 @@ def read_AniformModel():
 					E.append(tmpdf['col1'].iloc[i+1])
 					nu.append(tmpdf['col1'].iloc[i+2])
 
-		if len(tmpdf['col2'].str.match('OrthoElasticLD')):
-			OrtElasNum = tmpdf[tmpdf['col2'].str.match('OrthoElasticLD')].index
+		if len(tmpdf['col2'].str.fullmatch('OrthoElasticLD')):
+			OrtElasNum = tmpdf[tmpdf['col2'].str.fullmatch('OrthoElasticLD')].index
 			for i in OrtElasNum:
 				E1.append(tmpdf['col1'].iloc[i+1])
 				E2.append(tmpdf['col1'].iloc[i+2])
@@ -116,8 +116,8 @@ def read_AniformModel():
 		m = []
 		CVFn = []
 
-		if len(tmpdf['col2'].str.match('ViscousCrossLD')):
-			CrViscNum = tmpdf[tmpdf['col2'].str.match('ViscousCrossLD')].index
+		if len(tmpdf['col2'].str.fullmatch('ViscousCrossLD')):
+			CrViscNum = tmpdf[tmpdf['col2'].str.fullmatch('ViscousCrossLD')].index
 			for i in CrViscNum:
 				CvEta0.append(tmpdf['col1'].iloc[i+2])
 				EtaInf.append(tmpdf['col1'].iloc[i+3])
@@ -126,33 +126,13 @@ def read_AniformModel():
 
 		# Adhesion between plies
 		tension = []
-		if len(tmpdf['col2'].str.match('AdhesionILD')):
-			AdhNum = tmpdf[tmpdf['col2'].str.match('AdhesionILD')].index
+		if len(tmpdf['col2'].str.fullmatch('AdhesionILD')):
+			AdhNum = tmpdf[tmpdf['col2'].str.fullmatch('AdhesionILD')].index
 			for i in AdhNum:
 				tension.append(tmpdf['col1'].iloc[i+1])
 
-
-
-		#print (Param['Ps'][0])
-		#print(Ps,Cond,Conv)
-			#print(tmpdf.col0.values,file=f) #for k in range(0,df.shape[0]
-		#for k in u:
-			#print(s.loc[k])
-			#print(s.loc[k].reset_index(drop=True))
-			#print(s.loc['*material'].reset_index(drop=True))
-			#print(k)
-
-		#tmpdf['col1'].iloc[i+1]=cp.Uniform(-0.1,0.1).sample(1,"R")[0]*Ps[0]+Ps[0]
-		#tmpdf['col1'].iloc[i+6]+0.1*tmpdf['col1'].iloc[i+6] # Eta0
-		#params = ['material','tau0', 'c0', 'filmthickness', 'C', 'eta0', 'n', 'ap', \
-		#'bp', 'p0', 'alpha', 'coefficient', 'Tinf']
-		#s = pd.Series(tmpdf['col1'].values, tmpdf['col0'].values)
-		#u = np.unique(s.index.values).tolist()
-		#print(u)
-		#tmpDF=pd.concat([s.loc[k].reset_index(drop=True) for k in u], axis=1, keys=u)
-		#print(maxrowlen,maxrownum)
 def createDesignMatrix():
-		global DesgnPnt
+		global DPdf
 		DesgnPnt = dict()
 
 		percent = 0.1
@@ -185,13 +165,117 @@ def createDesignMatrix():
 						for j in range(0,len(Param[l])):
 							Mean=float(Param[l][j])
 							DesgnPnt[l].append((1+Udist.sample(sample_num,'R'))*Mean)
-							print(Mean,DesgnPnt)
+							#print(Mean,DesgnPnt)
 
 
-		with open('model'+str(sample_num)+'.afi','w') as f:
-			dfAsString=tmpdf.to_string(header=False, index=False)
+		DPdf = pd.DataFrame.from_dict(DesgnPnt,orient='index')
+
+		#dp = dp.append(DesgnPnt["Eta0"])
+		print (DPdf.head())
+		print (DPdf.tail())
+
+def write_AniformModel(var):
+
+		desgnId = var-1
+
+		AnInpdf = pd.DataFrame()
+		AnInpdf = tmpdf
+
+
+		print (AnInpdf.head())
+		print (AnInpdf.tail())
+
+		params = DPdf.index
+		params = list(map(lambda x: x.replace('Ps', 'penaltystiffness'), params))
+		params = list(map(lambda x: x.replace('Ft', 'filmthickness'), params))
+
+		for parnam in params:
+
+			if len(AnInpdf['col0'].str.fullmatch(parnam)):
+				row = AnInpdf[AnInpdf['col0'].str.fullmatch(parnam)].index
+				track=0
+				for count, r in enumerate(row):
+					#if (AnInpdf['col0'].iloc[row] == parnam)
+					#print('OK')
+					if parnam == 'penaltystiffness':
+						try:
+							print('\t Current value of',AnInpdf['col0'].iloc[r], AnInpdf['col1'].iloc[r])
+							AnInpdf['col1'].iloc[r] = DPdf[count].loc['Ps'][desgnId]
+							print('\t New value of', AnInpdf['col0'].iloc[r], AnInpdf['col1'].iloc[r])
+						except:
+							print("'None' type object found")
+							AnInpdf['col1'].iloc[r] = DPdf[0].loc['Ps'][desgnId] # It is assumed here \
+							# that in subsequent occurences of parnam in Input file, the corresponding \
+							# value of the parameter (Mean) is the same as that in the very first \
+							# occurence. This needs reconsideration as it can result in a bug when, \
+							# for eg: PenaltyPolymer is defined three times and for the first occurence \
+							# value of Ps is X and for the two subsequent occurencea the value is Y
+					elif parnam == 'filmthickness':
+						try:
+							print('\t Current value of',AnInpdf['col0'].iloc[r], AnInpdf['col1'].iloc[r])
+							AnInpdf['col1'].iloc[r] = DPdf[count].loc['Ft'][desgnId]
+							print('\t New value of', AnInpdf['col0'].iloc[r], AnInpdf['col1'].iloc[r])
+						except:
+							print("'None' type object found")
+							AnInpdf['col1'].iloc[r] = DPdf[0].loc['Ft'][desgnId]
+
+					elif parnam == 'eta0': #
+						if AnInpdf['col2'].iloc[r-6] == 'PenaltyPolymerILD':
+							print('Ok', AnInpdf['col2'].iloc[r-6], 'row', r)
+							try:
+								print('\t Current value of',AnInpdf['col0'].iloc[r], AnInpdf['col1'].iloc[r])
+								AnInpdf['col1'].iloc[r] = DPdf[count-track].loc[parnam][desgnId]
+								print('\t New value of', AnInpdf['col0'].iloc[r], AnInpdf['col1'].iloc[r])
+							except:
+								print("'None' type object found")
+								AnInpdf['col1'].iloc[r] = DPdf[0].loc[parnam][desgnId]
+						else:
+							track = track+1
+							print('This eta0 is associated with ViscousCrossLD.','track = %.0d' %(track))
+
+					elif parnam == 'n': #
+
+						if AnInpdf['col2'].iloc[r-7] == 'PenaltyPolymerILD':
+							print('Ok', AnInpdf['col2'].iloc[r-7], 'row', r, parnam)
+							try:
+								print('\t Current value of',AnInpdf['col0'].iloc[r], AnInpdf['col1'].iloc[r])
+								AnInpdf['col1'].iloc[r] = DPdf[count-track].loc[parnam][desgnId]
+								print('\t New value of', AnInpdf['col0'].iloc[r], AnInpdf['col1'].iloc[r])
+							except:
+								print("'None' type object found")
+								AnInpdf['col1'].iloc[r] = DPdf[0].loc[parnam][desgnId]
+						else:
+							track = track+1
+							print("This 'n' is associated with ViscousCrossLD.",'track = %.0d' %(track))
+
+					else:
+						try:
+							print('\t Current value of',AnInpdf['col0'].iloc[r], AnInpdf['col1'].iloc[r])
+							AnInpdf['col1'].iloc[r] = DPdf[count].loc[parnam][desgnId]
+							print('\t New value of', AnInpdf['col0'].iloc[r], AnInpdf['col1'].iloc[r])
+						except:
+							print("'None' type object found")
+							AnInpdf['col1'].iloc[r] = DPdf[0].loc[parnam][desgnId]
+
+		# Write input file for current design point
+		print('Current working directory', os.getcwd())
+		print('Changing to the subdirectory for current sample')
+
+		file_path = "./sample_%.0f/file.txt" %(var)
+		directory = os.path.dirname(file_path)
+
+		if not os.path.exists(directory): # check if the sample exist
+			os.makedirs(directory) # make directory for the sample
+		os.chdir('sample_'+str(var))
+
+		with open(filename+str(var)+'.afi','w') as f:
+			dfAsString=AnInpdf.to_string(header=False, index=False)
 			f.write(dfAsString)
 		f.close()
+
+		os.chdir('..')
+
+
 
 def run_Aniform(sampleID):
 
@@ -208,7 +292,7 @@ def run_Aniform(sampleID):
 		path = "./sample_%.0f"%(sampleID)
 
 		wd = os.getcwd()
-		meshsubdir = wd+'meshes'
+		meshsubdir = wd+'\meshes'
         #print('Current working directory',meshsubdir)
 		print('Current working directory',wd)
 		try:
@@ -219,9 +303,14 @@ def run_Aniform(sampleID):
 
 		os.chdir(path)
 		print('Current working directory',os.getcwd())
-		print('Aniform output file exist?',)
-		print('Running Aniform')
-		#call("AniformP " +inpfilename,shell=True)
+
+		print('Check if Aniform output file exist?',)
+
+		outfilename = filename+str(sampleID)+'.out'
+		if not os.path.exists(outfilename):
+				print('Running Aniform: filename', filename+str(sampleID)+'.afi')
+				call("AniformP " +filename+str(sampleID)+'.afi',shell=True)
+		else: print('Output file exists')
 
 		os.chdir('..')
 		print('Current working directory',os.getcwd())
@@ -245,6 +334,7 @@ def main(args):
 		samples=[i for i in range(0, sample_num)]
 
 		for s in samples:
+				write_AniformModel(s+1)
 				run_Aniform(s+1)
 
 if __name__ == '__main__':
